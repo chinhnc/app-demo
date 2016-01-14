ba4133a8221e3abc0e9bd455fad0aa688213d272
chinhnc <chinhharubn@gmail.com>
2016-01-14 10:28:14 +0700

    fix bug like & comment permissions

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,11 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+    @user = User.new(name: "Example User234213421", email: "user1234234@gmail.com", activated:true)
+  end
+  test "should be valid" do
+    assert @user.valid?
+  end
 end
