21eea5b7a254d736cee0c345e66ec6bacc70c3fc
chinhnc <chinhharubn@gmail.com>
2016-03-16 22:05:07 +0700

    update test code

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -4,4 +4,21 @@ class MicropostTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @user = users(:michael)
+    @micropost = @user.microposts.build(content: "The first micropost")
+  end
+
+  test "should be valid" do
+    assert @micropost.valid?
+  end
+
+  test "user id should be present" do
+    @micropost.user_id = nil
+    assert_not @micropost.valid?
+  end
+
+  test "order should bu most recent first" do
+    assert_equal Micropost.first, microposts(:most_recent)
+  end
 end
